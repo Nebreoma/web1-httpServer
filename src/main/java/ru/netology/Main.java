c9c27d6ac224d6eda8a4cd025bec7ed603973604
Eva <nebreoma12@yandex.ru>
2024-10-23 00:36:09 +0300

    Add files in branch feature/query

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,14 +1,42 @@
 package ru.netology;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 public class Main {
-    public static void main(String[] args) {
-        ExecutorService threadPool = Executors.newFixedThreadPool(64);
-        final var server = new Server();
 
-        server.activate(threadPool);
-        server.listen(9999);
+    public static void main(String[] args) {
+
+        final int SERVER_PORT = 9999;
+        final int THREAD_POOL_SIZE = 64;
+
+        Server server = new Server();
+        server.start(SERVER_PORT, THREAD_POOL_SIZE);
+
+        server.addHandler("GET", "/messages", (request, responseStream) -> {
+
+            // TODO: handlers code
+            String response = "{\"/messages\":  [\"Hello, World!\"]}";
+            responseStream.write(("HTTP/1.1 200 OK\r\n" +
+                    "Content-Type: application/json\r\n" +
+                    "Content-Length: " + response.length() + "\r\n" +
+                    "Connection: close\r\n" +
+                    "\r\n").getBytes());
+            responseStream.write(response.getBytes());
+            responseStream.flush();
+        });
+        server.addHandler("POST", "/messages", (request, responseStream) -> {
+            // TODO: handlers code
+            String response = "{\"messages\": [\"Hello, World!\"]}";
+            responseStream.write(("HTTP/1.1 200 OK\r\n" +
+                    "Connection: close\r\n" +
+                    "\r\n").getBytes());
+            responseStream.write(response.getBytes());
+            responseStream.flush();
+        });
+
+//        server.start(SERVER_PORT, THREAD_POOL_SIZE);
+//
+        server.listen();
+
+        server.stop();
+
     }
 }
--- src/main/java/ru/netology/Server.java
+++ src/main/java/ru/netology/Server.java
@@ -1,97 +1,131 @@
 package ru.netology;
 
+import java.io.*;
 import java.net.ServerSocket;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.time.LocalDateTime;
+import java.net.URISyntaxException;
 import java.util.List;
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Server {
-    final static List validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");;
-    static ExecutorService threadPool;
 
-    //методами для: запуска;
-    public static void activate(ExecutorService threadPool) {
-        Server.threadPool = threadPool;
+    private final List<String> validPaths = List.of("/index.html",
+            "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js",
+            "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
+
+    static private ServerSocket serverSocket;
+    static int SERVER_PORT;
+    static private ExecutorService threadPool;
+    static private ConcurrentHashMap<String, Map<String, Handler>> handlers;
+
+
+    //запуск сервера
+    public void start(int serverPort, int threadPoolSize) {
+//        try {
+        SERVER_PORT = serverPort;
+        threadPool = Executors.newFixedThreadPool(threadPoolSize);
+        handlers = new ConcurrentHashMap<>();
+        System.out.println("Server start!");
+
+//        } catch (IOException e) {
+//            System.out.println("Ошибка при запуске сервера!");
+//        }
     }
 
-    //метод для: обработки конкретного подключения.
-    public static void listen(int port) { //9999
-        try (final var serverSocket = new ServerSocket(port)) {
-            while (true) {
-                threadPool.execute(() -> {
-                    try (
-                            final var socket = serverSocket.accept();
-                            final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                            final var out = new BufferedOutputStream(socket.getOutputStream());
-                    ) {
-                        // read only request line for simplicity
-                        // must be in form GET /path HTTP/1.1
-                        final var requestLine = in.readLine();
-                        final var parts = requestLine.split(" ");
 
-                        if (parts.length != 3) {
-                            // just close socket
-                            Thread.currentThread().interrupt();
-                        }
-
-                        final var path = parts[1];
-                        if (!validPaths.contains(path)) {
-                            out.write((
-                                    "HTTP/1.1 404 Not Found\r\n" +
-                                            "Content-Length: 0\r\n" +
-                                            "Connection: close\r\n" +
-                                            "\r\n"
-                            ).getBytes());
-                            out.flush();
-                            Thread.currentThread().interrupt();
-                        }
-
-                        final var filePath = Path.of(".", "public", path);
-                        final var mimeType = Files.probeContentType(filePath);
-
-                        // special case for classic
-                        if (path.equals("/classic.html")) {
-                            final var template = Files.readString(filePath);
-                            final var content = template.replace(
-                                    "{time}",
-                                    LocalDateTime.now().toString()
-                            ).getBytes();
-                            out.write((
-                                    "HTTP/1.1 200 OK\r\n" +
-                                            "Content-Type: " + mimeType + "\r\n" +
-                                            "Content-Length: " + content.length + "\r\n" +
-                                            "Connection: close\r\n" +
-                                            "\r\n"
-                            ).getBytes());
-                            out.write(content);
-                            out.flush();
-                            Thread.currentThread().interrupt();
-                        }
-
-                        final var length = Files.size(filePath);
-                        out.write((
-                                "HTTP/1.1 200 OK\r\n" +
-                                        "Content-Type: " + mimeType + "\r\n" +
-                                        "Content-Length: " + length + "\r\n" +
-                                        "Connection: close\r\n" +
-                                        "\r\n"
-                        ).getBytes());
-                        Files.copy(filePath, out);
-                        out.flush();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                        Thread.currentThread().interrupt();
-                    }
-                });
-            }
+    //остановка сервера
+    public void stop() {
+        try {
+            threadPool.shutdown();
+            serverSocket.close();
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Ошибка при остановке сервера!");
         }
-    }//конец метода
-}//конец класса
+    }
+
+    //работа сервера
+    public void listen() {
+        try {
+            serverSocket = new ServerSocket(SERVER_PORT);
+
+            while (!serverSocket.isClosed()) {
+                try {
+                    final var socket = serverSocket.accept();
+                    threadPool.execute(() -> {
+                        try (final var in = new BufferedInputStream(socket.getInputStream());
+                             final var out = new BufferedOutputStream(socket.getOutputStream())) {
+                            System.out.println("Подключение клиента!");
+                            connected(in, out);
+                            System.out.println("Запуск потока!");
+                        } catch (IOException ex) {
+                            System.out.println("Ошибка при подключении потока клиента!");
+                            Thread.currentThread().interrupt();
+                        }
+                    });
+                } catch (IOException ex) {
+                    System.out.println("Ошибка при подключении потока клиента!");
+                    break;
+                }
+            } //конец while
+
+        } catch (IOException e) {
+            System.out.println("Ошибка при запуске сервера!");
+        }
+    }
+
+
+    public void addHandler(String method, String path, Handler handler) {
+
+        if (!handlers.containsKey(method)) {
+            handlers.put(method, new ConcurrentHashMap<>());
+        }
+        handlers.get(method).put(path, handler);
+    }
+
+
+    private static void connected(BufferedInputStream in, BufferedOutputStream out) {
+
+        try {
+            System.out.println("Внутри потока1");
+            Request request = Request.createRequest(in, out);
+            System.out.println("Внутри потока2");
+            // Check for bad requests and drop connection
+
+            if (request == null || !handlers.containsKey(request.getMethod())) {
+                badRequest(out);
+                Thread.currentThread().interrupt();
+            } else {
+                Handler handler = handlers.get(request.getMethod()).get(request.getPath());
+                handler.handle(request, out);
+//            } else {
+//                out.write((
+//                        "HTTP/1.1 200 OK\r\n" +
+//                                "Content-Length: 0\r\n" +
+//                                "Connection: close\r\n" +
+//                                "\r\n"
+//                ).getBytes());
+//                out.flush();
+            }
+
+        } catch (IOException | URISyntaxException e) {
+            System.out.println("Ошибка!!!");
+            Thread.currentThread().interrupt();
+        }
+
+    }//конец метода connected
+
+
+    public static void badRequest(BufferedOutputStream out) throws IOException {
+        out.write((
+                "HTTP/1.1 400 Bad Request\r\n" +
+                        "Content-Length: 0\r\n" +
+                        "Connection: close\r\n" +
+                        "\r\n"
+        ).getBytes());
+        out.flush();
+        Thread.currentThread().interrupt();
+    }
+
+}
